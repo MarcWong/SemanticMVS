2cda13283cb3c3455eb4e870c8ed07293565c2e5
marcWong <yaowang95@pku.edu.cn>
2020-03-08 14:54:56 +0800

    fix colormap code

--- kdtree.sh
+++ kdtree.sh
@@ -1,5 +1,6 @@
-python kdtree.py --type=0 --batch_size=10 --resolution_level=2
-python kdtree.py --type=3 --batch_size=10 --K=10 --resolution_level=2
+python kdtree.py --type=0 --batch_size=100 --resolution_level=2
+python kdtree.py --type=3 --batch_size=100 --K=15 --resolution_level=2
+# python kdtree.py --type=3 --batch_size=10 --K=10 --resolution_level=2
 # python kdtree.py --type=3 --batch_size=1 --K=10 --resolution_level=2
 # python kdtree.py --type=3 --batch_size=1 --K=8 --resolution_level=2
 # python kdtree.py --type=3 --batch_size=1 --K=6 --resolution_level=2
@@ -7,7 +8,7 @@ python kdtree.py --type=3 --batch_size=10 --K=10 --resolution_level=2
 # python kdtree.py --type=3 --batch_size=1 --K=4 --resolution_level=2
 # python kdtree.py --type=3 --batch_size=1 --K=20 --resolution_level=2
 # python kdtree.py --type=1 --batch_size=10 --K=5 --resolution_level=2
-python kdtree.py --type=2 --batch_size=10 --K=5 --resolution_level=2
+# python kdtree.py --type=2 --batch_size=10 --K=5 --resolution_level=2
 # python kdtree.py --type=1 --batch_size=2 --K=15 --resolution_level=2
 # python kdtree.py --type=2 --batch_size=2 --K=15 --resolution_level=2
 # python kdtree.py --type=1 --batch_size=5 --K=15 --resolution_level=2
@@ -18,5 +19,5 @@ python kdtree.py --type=2 --batch_size=10 --K=5 --resolution_level=2
 # python kdtree.py --type=2 --batch_size=25 --K=15 --resolution_level=2
 # python kdtree.py --type=1 --batch_size=50 --K=15 --resolution_level=2
 # python kdtree.py --type=2 --batch_size=50 --K=15 --resolution_level=2
-# python kdtree.py --type=1 --batch_size=100 --K=15 --resolution_level=2
-# python kdtree.py --type=2 --batch_size=100 --K=15 --resolution_level=2
+python kdtree.py --type=1 --batch_size=100 --K=15 --resolution_level=2
+python kdtree.py --type=2 --batch_size=100 --K=15 --resolution_level=2
--- util/changeColorMap.py
+++ util/changeColorMap.py
@@ -2,19 +2,16 @@ from util import writePointCloud
 old_colormap = [(0,255,0),(0,0,255),(255,0,0),(0,255,255),(0,0,0)] #BGR sequence
 new_colormap = [(35,142,107),(70,70,70),(128,64,128),(142,0,0),(0,0,0)] #BGR sequence
 
-path="/data1/Dataset/pku/m1_semantic/"
-file1 = open(path + "semantic/backup/scene_dense_softmax_k=3.obj")
+path="/data1/Dataset/game/Tianjin_semantic/"
+filepath = "semantic/scene_dense_softmax_k=4.obj"
+file1 = open(path + filepath)
 
-x = []
-y = []
-z = []
+xyz = []
 p = []
 line = file1.readline()
 while line:
     dt = line.split()
-    x.append(float(dt[1]))
-    y.append(float(dt[2]))
-    z.append(float(dt[3]))
+    xyz.append([float(dt[1]), float(dt[2]), float(dt[3])])
     flag = 0
     for i in range(len(old_colormap)):
         if int(dt[6]) == old_colormap[i][0] and int(dt[5]) == old_colormap[i][1] and int(dt[4]) == old_colormap[i][2]:
@@ -26,4 +23,4 @@ while line:
 
 file1.close()
 
-writePointCloud(x, y, z, p, path + "semantic/scene_dense_softmax_k=3.obj")
+writePointCloud(xyz, p, path + "semantic/change_scene_dense_softmax_k=4.obj")
