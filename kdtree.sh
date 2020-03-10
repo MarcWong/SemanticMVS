33c2791686b21a244552f5863c7900abf0d51ab8
marcWong <yaowang95@pku.edu.cn>
2020-03-10 15:36:54 +0800

    update HU-Hall params

--- kdtree.sh
+++ kdtree.sh
@@ -1,23 +1,4 @@
-python kdtree.py --type=0 --batch_size=100 --resolution_level=2
-python kdtree.py --type=3 --batch_size=100 --K=15 --resolution_level=2
-# python kdtree.py --type=3 --batch_size=10 --K=10 --resolution_level=2
-# python kdtree.py --type=3 --batch_size=1 --K=10 --resolution_level=2
-# python kdtree.py --type=3 --batch_size=1 --K=8 --resolution_level=2
-# python kdtree.py --type=3 --batch_size=1 --K=6 --resolution_level=2
-# python kdtree.py --type=3 --batch_size=1 --K=5 --resolution_level=2
-# python kdtree.py --type=3 --batch_size=1 --K=4 --resolution_level=2
-# python kdtree.py --type=3 --batch_size=1 --K=20 --resolution_level=2
-# python kdtree.py --type=1 --batch_size=10 --K=5 --resolution_level=2
-# python kdtree.py --type=2 --batch_size=10 --K=5 --resolution_level=2
-# python kdtree.py --type=1 --batch_size=2 --K=15 --resolution_level=2
-# python kdtree.py --type=2 --batch_size=2 --K=15 --resolution_level=2
-# python kdtree.py --type=1 --batch_size=5 --K=15 --resolution_level=2
-# python kdtree.py --type=2 --batch_size=5 --K=15 --resolution_level=2
-# python kdtree.py --type=1 --batch_size=20 --K=15 --resolution_level=2
-# python kdtree.py --type=2 --batch_size=20 --K=15 --resolution_level=2
-# python kdtree.py --type=1 --batch_size=25 --K=15 --resolution_level=2
-# python kdtree.py --type=2 --batch_size=25 --K=15 --resolution_level=2
-# python kdtree.py --type=1 --batch_size=50 --K=15 --resolution_level=2
-# python kdtree.py --type=2 --batch_size=50 --K=15 --resolution_level=2
-python kdtree.py --type=1 --batch_size=100 --K=15 --resolution_level=2
-python kdtree.py --type=2 --batch_size=100 --K=15 --resolution_level=2
+# python kdtree.py --type=0 --batch_size=10 --resolution_level=2
+# python kdtree.py --type=1 --batch_size=10 --K=15 --resolution_level=2
+python kdtree.py --type=2 --batch_size=10 --K=15 --resolution_level=2
+#　python kdtree.py --type=3 --batch_size=10 --K=15 --resolution_level=2
