27afdf8f70b5c8cec3b92a8434cd5ed5e6ed0a91
marcWong <yaowang95@pku.edu.cn>
2020-03-16 23:32:03 +0800

    dataset update

--- util/util.py
+++ util/util.py
@@ -27,19 +27,32 @@ def writePointCloud(points_new, p_new, path):
         file3.write('v ' + str(points_new[i][0]) + ' ' + str(points_new[i][1]) + ' ' + str(points_new[i][2]) + ' ' + str(p_new[i][2]) + ' ' + str(p_new[i][1]) + ' ' + str(p_new[i][0]) + '\n')
     file3.close()
 
+def readfile(filename):
+    fx = []
+    with open(filename) as f:
+        for line in f.readlines():
+            line_str = line.strip()
+            line_list = line_str.split(",")
+            for i in line_list:
+                if i:
+                    fx.append(int(i))
 
-fig = plt.figure(figsize=(16,12))
-x = (4, 5, 6, 8, 10, 15, 20)
-y1 = [87.72, 87.73, 87.74, 87.89, 87.91, 88.23, 87.32]
-y2 = [87.64, 87.62, 87.62, 87.84, 87.86, 88.14, 87.37]
-plt.plot(x,y1,label='Prob')
-plt.plot(x,y2,label='Argmax')
-plt.xticks(x,fontsize=36)#设置x的刻度
-plt.yticks(fontsize=36)#设置x的刻度
-# plt.xticks(x[::2])
-plt.ylabel('Accuracy(%)',fontsize=40)
-plt.xlabel('k',fontsize=40)
+    return fx
 
-plt.legend(loc=2, ncol=3, fontsize=34)
+def draw_figure():
+    fig = plt.figure(figsize=(16,12))
+    x = (4, 5, 6, 8, 10, 15, 20)
+    y1 = [87.72, 87.73, 87.74, 87.89, 87.91, 88.23, 87.32]
+    y2 = [87.64, 87.62, 87.62, 87.84, 87.86, 88.14, 87.37]
+    plt.plot(x,y1,label='Prob')
+    plt.plot(x,y2,label='Argmax')
+    plt.xticks(x,fontsize=36)#设置x的刻度
+    plt.yticks(fontsize=36)#设置x的刻度
+    # plt.xticks(x[::2])
+    plt.ylabel('Accuracy(%)',fontsize=40)
+    plt.xlabel('k',fontsize=40)
 
-plt.savefig('ablation_k.png', format='png')
+    plt.legend(loc=2, ncol=3, fontsize=34)
+
+    plt.savefig('ablation_k.png', format='png')
+
